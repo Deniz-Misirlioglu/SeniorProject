5d3b3c68c9f7a34bdb87da664b5aff24cc03c176
taylo550Riley <taylo550@miamioh.edu>
2023-09-24 22:58:59 -0400

    first multiplayer commit - top down

---
+++ HideAndSeek/Source/HideAndSeek/HideAndSeek.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HideAndSeek.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HideAndSeek, "HideAndSeek" );
+
+DEFINE_LOG_CATEGORY(LogHideAndSeek)
+ 
