e5f1869a15ee25dd5091b209dde2eb370988364d
taylo550Riley <taylo550@miamioh.edu>
2024-01-17 18:53:51 -0500

    first commit with content

---
+++ HideAndSeek/Plugins/AdvancedSteamSessions/Intermediate/Build/Win64/UnrealGame/Inc/AdvancedSteamSessions/UHT/AdvancedSteamSessionsClasses.h
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#pragma once
+
+
+#include "../../../../../../UnrealProjects/SeniorProject/HideAndSeek/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/AdvancedSteamFriendsLibrary.h"
+#include "../../../../../../UnrealProjects/SeniorProject/HideAndSeek/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/AdvancedSteamWorkshopLibrary.h"
+#include "../../../../../../UnrealProjects/SeniorProject/HideAndSeek/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/SteamRequestGroupOfficersCallbackProxy.h"
+#include "../../../../../../UnrealProjects/SeniorProject/HideAndSeek/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/SteamWSRequestUGCDetailsCallbackProxy.h"
+
